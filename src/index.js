a3e51c2fea7d5e47bcfff9e6b58c1dbf882b2b63
Taewoo Lee <lteawoo@naver.com>
2020-04-29 11:23:16 +0900

    separate and minimalize false

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import moment from "moment"
-
-function currentTime() {
-    /* const date = new Date()
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
-    return `${hours}:${minutes}:${seconds}` */
-    return moment().format("H:m:s")
-}
+import { currentTime } from "./time"
 
 const div = document.createElement("div")
 document.body.appendChild(div)
